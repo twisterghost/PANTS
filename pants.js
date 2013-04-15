c6c8715fa4586afe9d20fa7d16bf53b78f2b7989
Michael Barrett <Michael@AmpquotVPS.com>
2013-04-15 19:24:21 -0400

    Added pants.js and webworker package

---
+++ pants.js
@@ -0,0 +1,15 @@
+var Worker = require('webworker-threads').Worker;
+// var w = new Worker('worker.js'); // Standard API
+
+ // You may also pass in a function:
+ var worker = new Worker(function(){
+   postMessage("I'm working before postMessage('ali').");
+     onmessage = function(event) {
+         postMessage('Hi ' + event.data);
+             self.close();
+         };
+});
+worker.onmessage = function(event) {
+  console.log("Worker said : " + event.data);
+};
+worker.postMessage('ali');
